b6d3dbe737cf20fcb49a2212167f587f2f2f466b
agostina <110134414+agostinaMagali@users.noreply.github.com>
2026-06-26 19:18:46 -0300

    Implementación de cartelera y validación antirroturas

--- cine.py
+++ cine.py
@@ -15,3 +15,28 @@ def menu_principal():
 
 if __name__ == "__main__":
     menu_principal()
+
+    recaudacion_total = 0.0
+total_entradas_vendidas = 0
+
+peliculas = {
+    "1": ["Toy Story 5", 40, 0, 1500.0, "16:00 HS"],
+    "2": ["Scary Movie 6", 30, 0, 1400.0, "22:30 HS"],
+    "3": ["El Diablo Viste a la Moda 2", 35, 0, 1600.0, "19:00 HS"]
+}
+
+def validar_entero(mensaje) -> int:
+    while True:
+        try:
+            valor = int(input(mensaje))
+            if valor > 0: return valor
+            print("❌ Error: Debe ser mayor a cero.")
+        except ValueError:
+            print("❌ Error: Ingrese un número entero.")
+
+def mostrar_cartelera():
+    print("\n🎬 --- CARTELERA ---")
+    for id_peli, datos in peliculas.items():
+        nombre, cap, ocup, prec, hor = datos
+        print(f"[{id_peli}] {nombre:<30} | {hor} | ${prec}")
+
